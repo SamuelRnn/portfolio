84ab6579c6ee39e4fecbcf9f397548faa0cbeaf6
SamuelRnn <ricardo.reinn@outlook.com>
2023-04-14 02:19:30 -0500

    head changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,14 +16,6 @@ export default function Home() {
 				<title>Samuel Perez | Full-stack Developer</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta name="description" content="Samuel Perez portfolio" />
-				<meta property="og:type" content="website" />
-				<meta
-					property="og:title"
-					content="Samuel Perez | Full-stack Developer"
-				/>
-				<meta property="og:url" content="https://samuelrnn.vercel.app/" />
-				<meta property="og:description" content="Samuel Perez portfolio" />
-				<meta property="og:image" content="https://i.imgur.com/KIrNTSN.png" />
 				<link rel="icon" href="/icon.svg" />
 			</Head>
 			<Toaster position="bottom-center" />
